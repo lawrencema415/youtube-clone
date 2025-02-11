c92fc1d8d656a1de765969c7e99848bf35156d56
Lawrence Ma <larry.ma@everbee.ca>
2025-02-10 19:13:31 -0800

    set up more trpc and upstash

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -1,12 +1,14 @@
 'use client';
 // ^-- to make sure we can mount the Provider from a server component
 import type { QueryClient } from '@tanstack/react-query';
+import superjson from 'superjson';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCReact } from '@trpc/react-query';
 import { useState } from 'react';
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
+
 export const trpc = createTRPCReact<AppRouter>();
 let clientQueryClientSingleton: QueryClient;
 function getQueryClient() {
@@ -39,7 +41,7 @@ export function TRPCProvider(
 		trpc.createClient({
 			links: [
 				httpBatchLink({
-					// transformer: superjson, <-- if you use a data transformer
+					transformer: superjson,
 					url: getUrl(),
 				}),
 			],
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { protectedProcedure, createTRPCRouter } from '../init';
+// import { auth } from '@clerk/nextjs/server';
+
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
